99706752c2daee4e794ea6894b8d1b7c416babae
KR <shiguma665hal@gmail.com>
2025-11-23 19:01:16 +0900

    update: メンバー選択画面の背景素材を差し替え

--- DX22_Project/MemberSelectBackGround.cpp
+++ DX22_Project/MemberSelectBackGround.cpp
@@ -3,6 +3,7 @@
 
 CMemberSelectBackGround::CMemberSelectBackGround()
 {
+	m_tParam.m_f3Pos = DirectX::XMFLOAT3(SCREEN_WIDTH * 0.5f, SCREEN_HEIGHT * 0.5f, 0.0f);
 	m_tParam.m_f3Size = DirectX::XMFLOAT3(SCREEN_WIDTH, SCREEN_HEIGHT, 0.0f);
 }
 
@@ -14,7 +15,7 @@ CMemberSelectBackGround::~CMemberSelectBackGround()
 void CMemberSelectBackGround::Init()
 {
 	CSpriteRenderer* pRenderer = AddComponent<CSpriteRenderer>();
-	pRenderer->Load(PATH_TEX("MemberSelectBack.jpg"));
+	pRenderer->Load(PATH_TEX("MemberSelectBack.png"));
 	pRenderer->LoadVertexShader(PATH_SHADER("VS_Sprite.cso"));
 	pRenderer->LoadPixelShader(PATH_SHADER("PS_Sprite.cso"));
 }
